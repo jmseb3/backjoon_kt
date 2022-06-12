a4e0483de99cc075b35a948dc8b46929f8b1d3c6
WonDDak <jmseb3@naver.com>
2022-06-12 23:07:23 +0900

    ch3 Update

--- BaekjoonChapter3.kt
+++ BaekjoonChapter3.kt
@@ -4,6 +4,7 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.lang.Exception
 import java.util.*
 
 class Chapter3 {
@@ -76,8 +77,6 @@ class Chapter3 {
 
     //https://www.acmicpc.net/problem/11021
     fun solution_11021() {
-        val bf = BufferedReader(InputStreamReader(System.`in`))
-        val bw = BufferedWriter(OutputStreamWriter(System.out))
         try {
             val n = bf.readLine().toInt()
             for (i in 1..n) {
@@ -91,8 +90,144 @@ class Chapter3 {
 
     }
 
+    //https://www.acmicpc.net/problem/11022
+    fun solution_11022() {
+        try {
+            val n = bf.readLine().toInt()
+            for (i in 1..n) {
+                val temp: List<String> = bf.readLine().split(" ")
+                bw.write("Case #$i: ${temp[0]} + ${temp[1]} = ${temp.sumOf { it.toInt() }}\n")
+            }
+            bw.flush()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    //https://www.acmicpc.net/problem/2438
+    fun solution_2438() {
+        try {
+            val n = bf.readLine().toInt()
+            for (i in 1..n) {
+                bw.write("*".repeat(i) + "\n")
+            }
+            bw.flush()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    //https://www.acmicpc.net/problem/2439
+    fun solution_2439() {
+        try {
+            val n = bf.readLine().toInt()
+            for (i in 1..n) {
+                bw.write(" ".repeat(n - i) + "*".repeat(i) + "\n")
+            }
+            bw.flush()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    //https://www.acmicpc.net/problem/10871
+    fun solution_10871() {
+        try {
+            var st = StringTokenizer(bf.readLine())
+            val n = st.nextToken().toInt()
+            val x = st.nextToken().toInt()
+
+            val sb = StringBuilder()
+            st = StringTokenizer(bf.readLine())
+
+            for (i in 0 until n) {
+                val value = st.nextToken().toInt()
+                if (value < x) sb.append(value).append(' ')
+            }
+            println(sb)
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    //https://www.acmicpc.net/problem/10952
+    fun solution_10952() {
+        try {
+            var flag = true
+            while (flag) {
+                val st = StringTokenizer(bf.readLine())
+                val n = st.nextToken().toInt()
+                val x = st.nextToken().toInt()
+
+                if (n == 0 && x == 0) {
+                    flag = false
+                    break
+                }
+                println(n + x)
+            }
+
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    //https://www.acmicpc.net/problem/10951
+    fun solution_10951() {
+        try {
+            var flag = true
+            while (flag) {
+                try {
+                    val st = StringTokenizer(bf.readLine())
+                    val n = st.nextToken().toInt()
+                    val x = st.nextToken().toInt()
+                    println(n + x)
+                } catch (e: Exception) {
+                    flag = false
+                    break
+                }
+
+            }
+
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    //https://www.acmicpc.net/problem/1110
+    fun solution_1110() {
+        try {
+            val n = bf.readLine().toInt()
+            var start = n
+            var cnt = 0
+            while (true) {
+                cnt ++
+                val first = start / 10
+                val second = start % 10
+                val sumX = first +second
+                val next = second*10 + sumX%10
+
+                if (next == n) {
+                    println(cnt)
+                    break
+                }
+                start = next
+            }
+
+
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+
 }
 
 fun main() {
-    Chapter3().solution_11021()
+    Chapter3().solution_1110()
 }
